8745d38a4f9ed2224310cf8c3742056ce2d2f12d
LetYana <114302100+LetYana@users.noreply.github.com>
2022-12-25 17:24:17 +0100

    final edit

--- js/js.js
+++ js/js.js
@@ -7,12 +7,47 @@
 //   $(".wrapper").addClass("active");
 // });
 
-function openBoxFunction(event) {
+function openBoxFunction1(event) {
   event.preventDefault();
   const image = document.querySelector("#present_1");
-  image.setAttribute("src", "img/present-2.png");
-  window.open("https://developer.mozilla.org", "_blank");
+  image.setAttribute("src", "img/present-1-openend.svg");
 }
 
-let giftBox = document.querySelector(".parallax_gift_1");
-giftBox.addEventListener("click", openBoxFunction);
+let giftBox1 = document.querySelector(".parallax_gift_1");
+giftBox1.addEventListener("click", openBoxFunction1);
+////
+function openBoxFunction2(event) {
+  event.preventDefault();
+  const image = document.querySelector("#present_2");
+  image.setAttribute("src", "img/present-2-opened.svg");
+}
+
+let giftBox2 = document.querySelector(".parallax_gift_2");
+giftBox2.addEventListener("click", openBoxFunction2);
+////
+function openBoxFunction3(event) {
+  event.preventDefault();
+  const image = document.querySelector("#present_3");
+  image.setAttribute("src", "img/present-3-opened.svg");
+}
+
+let giftBox3 = document.querySelector(".parallax_gift_3");
+giftBox3.addEventListener("click", openBoxFunction3);
+///
+function openBoxFunction4(event) {
+  event.preventDefault();
+  const image = document.querySelector("#present_4");
+  image.setAttribute("src", "img/present-4-opened.svg");
+}
+
+let giftBox4 = document.querySelector(".parallax_gift_4");
+giftBox4.addEventListener("click", openBoxFunction4);
+////
+function openBoxFunction5(event) {
+  event.preventDefault();
+  const image = document.querySelector("#present_5");
+  image.setAttribute("src", "img/present-5-opened.svg");
+}
+
+let giftBox5 = document.querySelector(".parallax_gift_5");
+giftBox5.addEventListener("click", openBoxFunction5);
